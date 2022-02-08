dabd758edef60e2b6da20b420dbfe44d9d1d287e
unknown <ashpreetbhatia99@gmail.com>
2022-02-08 14:03:40 +0530

    Bcrypt Hashing implemented

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-require('dotenv').config()
 const express = require("express")
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 const app = express();
 
 app.use(express.static("public"));
@@ -35,7 +37,7 @@ app.get("/login",function(req,res){
 
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
     User.findOne(
         {email : username},
         function(err,foundUser){
@@ -43,11 +45,13 @@ app.post("/login",function(req,res){
                 console.log(err);
             }else{
                 if(foundUser){
-                    if(foundUser.password === password){
-                        res.render("secrets");
-                    }else{
-                        res.send("User not found");
-                    }
+                    bcrypt.compare(password, foundUser.password, function(err, result) {
+                        if(result == true){
+                            res.render("secrets");
+                        }else{
+                            res.send("User not found.");
+                        }
+                    });
                 }
             }
             
@@ -57,18 +61,22 @@ app.post("/login",function(req,res){
 app.get("/register",function(req,res){
     res.render("register");
 })
-app.post("/register",function(req,res){     
-    const newUser = new User({
-        email : req.body.username,
-        password : md5(req.body.password)
+app.post("/register",function(req,res){
+    
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email : req.body.username,
+            password : hash
+        });
+        newUser.save(function(err){
+            if(!err){
+                res.render("secrets");
+            }else{
+                console.log(err);
+            }
+        });
     });
-    newUser.save(function(err){
-        if(!err){
-            res.render("secrets");
-        }else{
-            console.log(err);
-        }
-    });
+    
 })
 app.listen(3000,function(){
     console.log("Server started on port 3000");
